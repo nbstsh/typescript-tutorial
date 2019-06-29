611ad00c3954dc60ac89b5d49ac73fad206329f1
nbstsh <nbstsh@gmail.com>
2019-06-30 02:55:33 +0900

    lecture1

---
+++ typescript.js
@@ -0,0 +1,5 @@
+var sum = function (a, b) {
+    return a + b;
+};
+var answer = sum('hello', 2);
+console.log(answer);
---
+++ typescript.ts
@@ -0,0 +1,7 @@
+const sum = (a: number, b: number) => {
+	return a + b;
+};
+
+const answer = sum(1, 2);
+
+console.log(answer);
